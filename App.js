934f1ac9e2063d500945b431ba83787289e85c5a
celsopixain <celsopixain@gmail.com>
2019-07-04 10:35:38 -0500

    alteração na app.js

--- App.js
+++ App.js
@@ -1,19 +1,97 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { View, StatusBar } from 'react-native'
+import { createStore } from 'redux'
+import AddDeck from './components/AddDeck'
+import AddCard from './components/AddCard'
+import DeckLists from './components/DeckLists'
+import Deck from './components/Deck'
+import Question from './components/Question'
+import { white, black } from './utils/colors'
+import reducer from './reducers'
+import { Provider } from 'react-redux'
+import { Constants } from 'expo'
+import { createBottomTabNavigator, createStackNavigator } from 'react-navigation'
+import { setLocalNotification } from './utils/helpers'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Mobile Flash card app</Text>
-    </View>
-  );
+const store = createStore(reducer)
+
+UdaciStatusBar = ({ backgroundColor, ...props }) => {
+    return (
+        <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+            <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+        </View>
+    )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Tabs = createBottomTabNavigator({
+    DeckLists: {
+        screen: DeckLists,
+        navigationOptions: {
+            tabBarLabel: 'DECKS'
+        }
+    },
+    AddDeck: {
+        screen: AddDeck,
+        navigationOptions: {
+            tabBarLabel: 'ADD DECK'
+        }
+    }
+}, {
+    tabBarOptions: {
+        activeTintColor: black,
+        labelStyle: {
+            fontSize: 20,
+            paddingBottom: 10,
+            fontWeight: 'bold',
+        }
+    }
+})
+
+const Stack = createStackNavigator({
+    Home: {
+        screen: Tabs,
+        navigationOptions: {
+            header: null
+        }
+    },
+    AddDeck: {
+        screen: AddDeck
+    },
+    AddCard: {
+        screen: AddCard
+    },
+    Deck: {
+        screen: Deck
+    },
+    Question: {
+        screen: Question
+    }
+}, {
+    navigationOptions: {
+        headerTintColor: black,
+        headerTitleStyle: {
+            fontSize: 22,
+        }
+    },
+    cardStyle: {
+        backgroundColor: white
+    }
+})
+
+export default class App extends React.Component {
+    componentDidMount(){
+        setLocalNotification()
+    }
+    render() {
+        return (
+            <Provider store={createStore(reducer)}>
+                <View style={{ flex: 1 }}>
+                    <UdaciStatusBar backgroundColor={black} barStyle="light-content" />
+                    <Stack />
+                </View>
+            </Provider>
+        )
+    }
+}
+
+
